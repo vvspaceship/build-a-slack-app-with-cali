d11610c05e3199b27ca311c1d254791c3d3240a5
Cali Castle <cali@calicastle.com>
2020-05-16 17:37:24 -0700

    Add example

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,28 @@ const port = process.env.PORT || 3000
 
   console.log(`🚀 App is running on port ⚡ ${port} 🚀`)
 })()
+
+const api = {
+  cat: process.env.CAT_API_BASE_URL
+}
+
+app.event('app_mention', async ({ say, event }) => {
+  if (/:cat:/i.test(event.text)) {
+    const data = await (await fetch(api.cat + 'images/search')).json()
+    const { url, id } = data[0]
+    await say({
+      text: '',
+      blocks: [
+        {
+          type: 'image',
+          title: {
+            type: 'plain_text',
+            text: id
+          },
+          image_url: url,
+          alt_text: id
+        }
+      ]
+    })
+  }
+})
