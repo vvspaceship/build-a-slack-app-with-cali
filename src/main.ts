c8c3ded607e0e545835a55c58a9d8c4289732070
Cali Castle <cali@calicastle.com>
2020-05-20 12:59:07 -0700

    Finish demo

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,83 @@ const port = process.env.PORT || 3000
 
   console.log(`🚀 App is running on port ⚡ ${port} 🚀`)
 })()
+
+const apiUrl = process.env.API_BASE_URL || ''
+
+const grabImage = async () => {
+  const images = await (await fetch(`${apiUrl}images/search`)).json()
+  if (!images || images.length === 0) return null
+  return images[0].url
+}
+
+app.message(/:cat:/i, async ({ message, say }) => {
+  const url = await grabImage()
+  if (url === null) return
+
+  await say({
+    text: '',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `<@${message.user}> Here's your image`
+        }
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'Random Cat Image',
+          emoji: true
+        },
+        image_url: url,
+        alt_text: 'Random Cat Image'
+      }
+    ]
+  })
+})
+
+app.event('app_home_opened', async ({ context, event }) => {
+  const url = await grabImage()
+  if (url === null) return
+
+  const { user } = event
+  await app.client.views.publish({
+    token: context.botToken,
+    user_id: user,
+    view: {
+      type: 'home',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Here's your image`
+          }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'image',
+          title: {
+            type: 'plain_text',
+            text: 'Random Cat Image',
+            emoji: true
+          },
+          image_url: url,
+          alt_text: 'Random Cat Image'
+        }
+      ]
+    }
+  })
+  await app.client.chat.postMessage({
+    token: context.botToken,
+    channel: 'UEFNNK11T',
+    text: `<@${user}> has opened your app's home tab`
+  })
+})
